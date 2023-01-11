01e7e4d3a8d2e13e24e375e75207b3598d870277
lucasmateo8 <mateolucas990@gmail.com>
2023-01-11 00:14:15 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+class ProductManager{
+    constructor(){
+        this.products = []
+    }
+
+    addProducts(title,description,price,){
+        const producto = {
+            title,
+            description,
+            price,
+        }
+    }
+}
+
+
+/* 
+ let id = 1
+if (this.eventos.length !== 0) {
+    id = this.eventos[this.eventos.length - 1].id + 1
+  }
+  return id
+}
+#evaluarEvento(idEvento) {
+  return this.eventos.find((evento) => evento.id === idEvento)
+} */
